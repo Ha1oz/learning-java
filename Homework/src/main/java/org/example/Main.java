204677bb80f2d746d8f4a64a3f86e86957b8c7a0
Ha1oz <oleg.barkalov@gmail.com>
2023-04-26 10:11:14 +0300

    feat: add start code

--- Homework/src/main/java/org/example/Main.java
+++ Homework/src/main/java/org/example/Main.java
@@ -2,6 +2,23 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Ex1();
+        Ex2();
+        Ex3();
+        Ex4();
     }
+    private static void Ex1() {
+
+    }
+    private static void Ex2() {
+
+    }
+    private static void Ex3() {
+
+    }
+    private static void Ex4() {
+
+    }
+    
+
 }
