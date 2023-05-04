4ed7e5fe3b40eaf8005b8daae06f50aeae04abeb
Ha1oz <oleg.barkalov@gmail.com>
2023-05-04 17:16:18 +0300

    feat: homework 11 is done

--- Homework/src/main/java/org/example/Main.java
+++ Homework/src/main/java/org/example/Main.java
@@ -1,22 +1,75 @@
 package org.example;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        Ex1();
-        Ex2();
-        Ex3();
-        Ex4();
-    }
-    private static void Ex1() {
+        isLeapYear(2021);
+        isLeapYear(2024);
+        isLeapYear(1900);
 
-    }
-    private static void Ex2() {
+        whichVersionNeeds(0, 2023);
+        whichVersionNeeds(1, 2023);
+        whichVersionNeeds(0, 2021);
+        whichVersionNeeds(1, 2020);
 
+        System.out.println("Delivery time: " + checkDeliveryTime(19));
+        System.out.println("Delivery time: " + checkDeliveryTime(60));
+        System.out.println("Delivery time: " + checkDeliveryTime(100));
     }
-    private static void Ex3() {
+    private static void isLeapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(year + " is a leap year");
+            return;
+        }
 
+        System.out.println(year + " is not a leap year");
     }
-    private static void Ex4() {
 
+    private static void whichVersionNeeds(int clientOS, int productionYear) {
+        String version = "";
+
+        if (needLightVersion(productionYear)) {
+            version += "light ";
+        }
+
+        switch (clientOS) {
+            case 0 -> version += "android";
+            case 1 -> version += "IOS";
+            default -> throw new RuntimeException("Incorrect OS type.");
+        }
+
+        System.out.printf("Install the %s version of the app here.\n", version);
+    }
+
+    private static boolean needLightVersion(int productionYear) {
+        int currentYear = LocalDate.now().getYear();
+
+        if (productionYear > currentYear) {
+            throw new RuntimeException("The production year cannot be greater than the current year.");
+        }
+
+        return productionYear < currentYear;
+    }
+
+    private static int checkDeliveryTime(Integer deliveryDistance) {
+        if (deliveryDistance < 0) {
+            throw new RuntimeException("Delivery distance cannot be negative.");
+        }
+        if (deliveryDistance > 100) {
+            throw new RuntimeException("No delivery. Delivery distance is too much.");
+        }
+
+        int deliveryTime = 1;
+
+        if (deliveryDistance > 20) {
+            deliveryTime++;
+        }
+
+        if (deliveryDistance > 60) {
+            deliveryTime++;
+        }
+
+        return deliveryTime;
     }
 }
